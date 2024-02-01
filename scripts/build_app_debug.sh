86538d97461546e298a4b10c2205c14ec8514d1c
Josnelihurt Rodriguez <josnelihurt@gmail.com>
2024-01-31 16:13:11 -0800

    fixing build platform

--- scripts/build_app_debug.sh
+++ scripts/build_app_debug.sh
@@ -2,4 +2,6 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 PRJ_DIR=$SCRIPT_DIR/..
 mkdir -p $PRJ_DIR/bin
-GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -gcflags=all='-N -l' -o $PRJ_DIR/bin/main $PRJ_DIR/cmd/main/main.go
+# select the right build command for your platform
+# GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -gcflags=all='-N -l' -o $PRJ_DIR/bin/main $PRJ_DIR/cmd/main/main.go
+GOOS=linux GOARCH=arm64 CGO_ENABLED=0 go build -gcflags=all='-N -l' -o $PRJ_DIR/bin/main $PRJ_DIR/cmd/main/main.go
